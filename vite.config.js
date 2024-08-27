beba2df3cdaf3867f7d54c981bc6678c67b72532
omar <omarcasio717@gmail.com>
2024-08-27 13:46:13 +0300

    https

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://92.205.230.83:8080",
+        target: "https://92.205.230.83:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
