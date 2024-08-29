b8447013b8a8ca8e09dea728a5f6d5f0cfb3bf21
omar <omarcasio717@gmail.com>
2024-08-29 23:39:48 +0300

    providers requests (Payouts)

--- src/pages/ProvidersPayouts.jsx
+++ src/pages/ProvidersPayouts.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import TableComponent from "../components/Table";
+import {
+  boatRequestsColumns,
+  coversColumns,
+  payoutsColumns,
+  payoutsData,
+  providerRequestsColumns,
+  providerRequestsData,
+} from "../data/mockData";
 import useUserStore from "../stores/useUserStore";
-import { getAllCovers } from "../network/coverServices";
-import { payoutsColumns, payoutsData, reviewsColumns } from "../data/mockData";
+import usePaginationStore from "../stores/usePaginationStore";
+import { getAll } from "../network/network";
 
 // export const getAllCategories = async () => {
 //   const res = await axios.get("/api/categories");
@@ -13,14 +21,45 @@ const PayoutsRequests = () => {
   const [rows, setRows] = useState([]);
   const { token, logout } = useUserStore();
   const [loading, setLoading] = useState(false);
+  const { pageSize, page, setTotalPages, setPage, setPageSize } =
+    usePaginationStore();
+
+  useEffect(() => {
+    setPage(1);
+    setPageSize(10);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const data = await getAll(
+          token,
+          logout,
+          `https://dev.sailgloble.com/admin/provider-requests/payout?limit=${pageSize}&page=${page}`
+        );
+        setRows(data.data);
+        setTotalPages(data.pageCount);
+
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error getting Providers:", error);
+      }
+    };
+
+    fetchData();
+  }, [pageSize, page]);
 
   return (
     <>
       <TableComponent
-        to=""
-        rows={payoutsData}
-        columns={payoutsColumns}
+        to={"/admin"}
+        rows={rows}
+        columns={boatRequestsColumns}
         loading={loading}
+        Endpoint={`https://dev.sailgloble.com/admin/provider-requests/approve/payout/`}
       />
     </>
   );
