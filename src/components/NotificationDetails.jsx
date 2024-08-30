9d3c2a591e5d17b56c8ce22a9d4711a6c925d1a5
omar <omarcasio717@gmail.com>
2024-08-30 18:13:06 +0300

    Notifications-7

--- src/components/NotificationDetails.jsx
+++ src/components/NotificationDetails.jsx
@@ -1,9 +1,9 @@
-const NotificationDetails = ({ text, title }) => {
+const NotificationDetails = () => {
   // const content = JSON.parse(text?.notificationContent);
-  console.log("content.title  :", title);
-  // console.log("content.body  :", content.body);
+  // console.log("content.title  :", title);
+  // // console.log("content.body  :", content.body);
 
-  return title === true ? title : true;
+  return true;
 };
 
 export default NotificationDetails;
